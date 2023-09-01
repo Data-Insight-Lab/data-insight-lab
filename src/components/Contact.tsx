3a33e99df290e42fbf526e86a30b90fd4cdb8c87
Paulo <paulo.gasparin@datainsightlab.io>
2023-09-01 17:04:18 -0300

    commit 4

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,38 +1,26 @@
+import React from 'react';
 import { contact } from '@/constants/contact';
-import { HTMLProps } from 'react';
 import { Animation } from './Animation';
-
-interface Props extends HTMLProps<HTMLInputElement> {}
-
-function TextInput({ ...rest }: Props) {
-  return (
-    <input className="text-input border-[.5px] border-zinc-700" {...rest} />
-  );
-}
+import { ContactForm } from './ContactForm.jsx'; // Import the ContactForm component
 
 export function Contact() {
   return (
     <Animation>
       <div className="my-52 flex flex-col justify-center items-center px-4 max-w-lg mx-auto">
-        <h1 id= "contact" className="mb-10 text-2xl font-normal">{contact.title}</h1>
-        <form
-          action=""
-          className="w-full p-4 flex-2   flex flex-col gap-4 border rounded-3xl border-zinc-800"
-        >
-          <TextInput type="email" placeholder="email" />
-          <TextInput type="text" placeholder="name" />
-          <textarea
-            placeholder="Leave a message"
-            className="text-input min-h-[150px] border-[.5px] border-zinc-700"
-          />
-          
-          <button className="button mt-10">Send</button>
-        </form>
+        <h1 id="contact" className="mb-10 text-2xl font-normal">
+          {contact.title}
+        </h1>
+        <div className="w-full p-4 flex-2 flex flex-col gap-4 border rounded-3xl border-zinc-800">
+          {/* Render the ContactForm component */}
+          <ContactForm />
+        </div>
         <div className="mt-4">
-        <a href="https://whatsa.me/5548996095043">
-            <div className="bg-blue-500 p-4 rounded-lg text-white">Click here for more info</div>
+          <a href="https://whatsa.me/5548996095043">
+            <div className="bg-blue-500 p-4 rounded-lg text-white">
+              Click here for more info
+            </div>
           </a>
-          </div>
+        </div>
       </div>
     </Animation>
   );
