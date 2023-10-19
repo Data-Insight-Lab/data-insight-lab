7169e7d07c9ff5b409dcb9cca0e23001c0ad5550
Paulo <paulo.gasparin@datainsightlab.io>
2023-10-19 15:25:02 -0300

    c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />   
-        <Document></Document>
+        <Document/>
       </Head>
       <body className={inter.className}>
         <Header />
