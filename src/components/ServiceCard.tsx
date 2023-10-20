bc2b6f97c11d831d6197191fa915c666fdce044c
Paulo <paulo.gasparin@datainsightlab.io>
2023-10-20 16:13:57 -0300

    commit 15

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -10,10 +10,10 @@ export function ServiceCard({ title, content, icon }: Props) {
   const Icon: IconType = icon;
 
   return (
-    <div className="p-4 border rounded-2xl border-zinc-50rem glassmorphism md:min-h-[28rem] sm:min-h-[10rem] md:w-1/2 w-full mb-4">
-      <Icon className="h-14 w-14 md:h-20 md:w-20 rounded-xl text-white" />
-      <p className="text-lg font-medium my-2 md:text-xl text-base">{title}</p>
-      <p className="text-sm font-light md:text-base text-sm">{content}</p>
-    </div>
+    <div className="card">
+    <Icon className="icon" />
+    <p className="title">{title}</p>
+    <p className="content">{content}</p>
+  </div>
   );
 }
