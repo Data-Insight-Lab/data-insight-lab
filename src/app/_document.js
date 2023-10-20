3c545ba77ba95b2764b1f07bfc8c5df864048162
Paulo <paulo.gasparin@datainsightlab.io>
2023-10-20 15:33:50 -0300

    7

--- src/app/_document.js
+++ src/app/_document.js
@@ -8,12 +8,12 @@ export default function Document() {
     <Html>
       <Head>
         <noscript>
-          <Image
+          <Image>
             height="1"
             width="1"
             style={{ display: 'none' }}
             src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-          />
+          </Image>
         </noscript>
       </Head>
       <body>
